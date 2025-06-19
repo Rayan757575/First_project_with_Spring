0aee869dc6c0ba41fba7bc52b59ad4bda832ad51
Rayan757575 <rayancatapretastl@gmail.com>
2025-06-18 23:46:22 -0300

    Creating new files to good requisition pratices

---
+++ src/main/java/com/example/demo/requests/anime/AnimePostRequestBody.java
@@ -0,0 +1,8 @@
+package com.example.demo.requests.anime;
+
+import lombok.Data;
+
+@Data
+public class AnimePostRequestBody {
+    private String name;
+}
---
+++ src/main/java/com/example/demo/requests/anime/AnimePutRequestBody.java
@@ -0,0 +1,9 @@
+package com.example.demo.requests.anime;
+
+import lombok.Data;
+
+@Data
+public class AnimePutRequestBody {
+    private Long id;
+    private String name;
+}
