6413807b8e70b49f760c7e4e4c5a85663be06fb7
Rayan757575 <rayancatapretastl@gmail.com>
2025-06-30 23:21:45 -0300

    implementing Customized exceptions

--- src/main/java/com/example/demo/service/AnimeService.java
+++ src/main/java/com/example/demo/service/AnimeService.java
@@ -7,9 +7,7 @@ import com.example.demo.repository.AnimeRepository;
 import com.example.demo.requests.anime.AnimePostRequestBody;
 import com.example.demo.requests.anime.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
