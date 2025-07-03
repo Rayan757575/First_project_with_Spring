62899fdf9ecb2ec9af88e8d65121c8b18ee11951
Rayan757575 <rayancatapretastl@gmail.com>
2025-07-02 21:17:59 -0300

    Implementing Validation's method

--- src/main/java/com/example/demo/requests/anime/AnimePostRequestBody.java
+++ src/main/java/com/example/demo/requests/anime/AnimePostRequestBody.java
@@ -1,8 +1,11 @@
 package com.example.demo.requests.anime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The name of this anime can not be empty")
     private String name;
 }
